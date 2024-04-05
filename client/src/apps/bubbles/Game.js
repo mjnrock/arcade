1ecdd2ee6c34365d7ad6d232dbe52efca1ed26cd
LesPantsFancy <mjnrock@umich.edu>
2024-04-05 13:59:00 -0400

    wip

--- client/src/apps/bubbles/Game.js
+++ client/src/apps/bubbles/Game.js
@@ -5,15 +5,26 @@ import GameLoop from "./GameLoop";
 import KeyboardInput from "./input/Keyboard";
 import MouseInput from "./input/Mouse";
 
+import BubbleEntity from "./entities/Bubble";
+import BubbleComponent from "./components/Bubble";
+
 import ArcadeInputSystem from "./systems/ArcadeInputSystem";
 
 import Router from "./lib/message/Router";
 import WebSocketBrowserClient from "./lib/ws/WebSocketBrowserClient";
-import Message from "./lib/message/Message";
 
 export class Game {
 	constructor ({ fps = 60, ...pixi } = {}) {
 		this.id = uuid();
+
+		this.player = {
+			input: {
+				crosshairGraphic: new PIXI.Graphics(),
+				mask: {},
+				x: ~~(window.innerWidth / 2),
+				y: ~~(window.innerHeight / 2),
+			},
+		};
 
 		this.router = new Router({
 			game: this,
@@ -48,7 +59,8 @@ export class Game {
 			...pixi,
 		});
 
-		// DEBUG - Client Only
+		this.pixi.stage.addChild(this.player.input.crosshairGraphic);
+
 		window.addEventListener("resize", () => {
 			this.pixi.renderer.resize(window.innerWidth, window.innerHeight);
 		});
@@ -99,6 +111,81 @@ export class Game {
 	}
 
 	update(dt) {
+		const step = 11;
+		if(this.player.input.mask?.joystick?.UP) {
+			this.player.input.y -= step;
+		}
+		if(this.player.input.mask?.joystick?.DOWN) {
+			this.player.input.y += step;
+		}
+		if(this.player.input.mask?.joystick?.LEFT) {
+			this.player.input.x -= step;
+		}
+		if(this.player.input.mask?.joystick?.RIGHT) {
+			this.player.input.x += step;
+		}
+
+		if(this.player.input.x < 0) {
+			this.player.input.x = 0;
+		} else if(this.player.input.x > window.innerWidth) {
+			this.player.input.x = window.innerWidth;
+		}
+
+		if(this.player.input.y < 0) {
+			this.player.input.y = 0;
+		} else if(this.player.input.y > window.innerHeight) {
+			this.player.input.y = window.innerHeight;
+		}
+
+		const { buttons } = this.player.input.mask ?? {}
+		const { K1, K2, K3, K4, K11, K12 } = buttons ?? {};
+		const cursor = this.player.input;
+		if(K1 || K2 || K3) {
+			const quantity = 1;
+			const bubbles = BubbleEntity.Factory(quantity, () => ({
+				meta: {
+					ttl: 1000 * (Math.random() * 2 + 1),
+				},
+				components: [
+					new BubbleComponent({
+						x: cursor.x,
+						y: cursor.y,
+						vx: (Math.random() - 0.5) * 50,
+						vy: (Math.random() - 0.5) * 50,
+						r: Math.random() * 10 + 5,
+						color: `#ff${ Math.floor(Math.random() * 256).toString(16).padStart(2, "0") }${ Math.floor(Math.random() * 256).toString(16).padStart(2, "0") }`,
+					}),
+				],
+			}));
+
+			this.currentWorld?.addEntity(...bubbles);
+		}
+		if(K4 || K11 || K12) {
+			const bubbles = BubbleEntity.Factory(1, () => ({
+				meta: {
+					ttl: 1000 * (Math.random() * 3),
+				},
+				components: [
+					new BubbleComponent({
+						x: Math.random() * window.innerWidth,
+						y: Math.random() * window.innerHeight,
+						vx: (Math.random() - 0.5) * 75,
+						vy: (Math.random() - 0.5) * 75,
+						r: Math.random() * 50 + 5,
+						// color: `#${ Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0") }`,
+						color: `#${ Math.floor(Math.random() * 256).toString(16).padStart(2, "0") }${ Math.floor(Math.random() * 256).toString(16).padStart(2, "0") }ff`,
+					}),
+				],
+			}));
+
+			this.currentWorld?.addEntity(...bubbles);
+		}
+
+
+
+
+
+
 		this.currentWorld?.update({
 			game: this,
 			dt: dt / 1000,
@@ -106,6 +193,13 @@ export class Game {
 	}
 
 	render() {
+		this.player.input.crosshairGraphic.clear();
+		this.player.input.crosshairGraphic.lineStyle(2, 0x000000, 1);
+		this.player.input.crosshairGraphic.moveTo(this.player.input.x - 10, this.player.input.y);
+		this.player.input.crosshairGraphic.lineTo(this.player.input.x + 10, this.player.input.y);
+		this.player.input.crosshairGraphic.moveTo(this.player.input.x, this.player.input.y - 10);
+		this.player.input.crosshairGraphic.lineTo(this.player.input.x, this.player.input.y + 10);
+
 		this.currentWorld?.render({
 			game: this,
 		});
--- client/src/apps/bubbles/systems/ArcadeInputSystem.js
+++ client/src/apps/bubbles/systems/ArcadeInputSystem.js
@@ -1,6 +1,4 @@
 import System from "../lib/message/System";
-
-
 import BubbleEntity from "../entities/Bubble";
 import BubbleComponent from "../components/Bubble";
 
@@ -11,52 +9,17 @@ export class ArcadeInputSystem extends System {
 
 	input({ data, message } = {}) {
 		const { buttons, joystick } = data;
-
-		const { K1, K2, K3, K4, K11, K12 } = buttons;
-
-		if(K1 || K2 || K3 || K4 || K11 || K12) {
-			const bubbles = BubbleEntity.Factory(1, () => ({
-				meta: {
-					ttl: 1000 * (Math.random() * 5 + 3),
-				},
-				components: [
-					new BubbleComponent({
-						x: Math.random() * window.innerWidth,
-						y: Math.random() * window.innerHeight,
-						vx: (Math.random() - 0.5) * 75,
-						vy: (Math.random() - 0.5) * 75,
-						r: Math.random() * 50 + 5,
-						// color: `#${ Math.floor(Math.random() * 16777215).toString(16).padStart(6, "0") }`,
-						color: `#ff${Math.floor(Math.random() * 256).toString(16).padStart(2, "0")}${Math.floor(Math.random() * 256).toString(16).padStart(2, "0")}`,
-					}),
-				],
-			}));
-
-			this.game.currentWorld?.addEntity(...bubbles);
-		}
-
+		const { K1, K2, K3, K4, K11, K12, SE, ST } = buttons;
 		const { UP, DOWN, LEFT, RIGHT } = joystick;
 
-		if(UP || DOWN || LEFT || RIGHT) {
-			const qty = ~~(Math.random() * 5 + 1);
-			const bubbles = BubbleEntity.Factory(qty, () => ({
-				meta: {
-					ttl: 1000 * (Math.random() * 5 + 3),
-				},
-				components: [
-					new BubbleComponent({
-						x: Math.random() * window.innerWidth,
-						y: Math.random() * window.innerHeight,
-						vx: (Math.random() - 0.5) * 75,
-						vy: (Math.random() - 0.5) * 75,
-						r: Math.random() * 50 + 5,
-						color: `#${ Math.floor(Math.random() * 256).toString(16).padStart(2, "0") }${ Math.floor(Math.random() * 256).toString(16).padStart(2, "0") }ff`,
-					}),
-				],
-			}));
+		const cursor = this.game.player.input;
 
-			this.game.currentWorld?.addEntity(...bubbles);
+		if(ST) {
+			this.game.player.input.x = ~~(Math.random() * window.innerWidth);
+			this.game.player.input.y = ~~(Math.random() * window.innerHeight);
 		}
+
+		this.game.player.input.mask = data;
 	}
 };
 
