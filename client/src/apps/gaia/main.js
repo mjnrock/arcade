c809e2e7b849af005a4cbe7bc70aa0d9764de1fd
LesPantsFancy <mjnrock@umich.edu>
2024-05-06 15:05:50 -0400

    rf

---
+++ client/src/apps/gaia/main.js
@@ -0,0 +1,28 @@
+import RPGGame from "../../modules/rpg/Game";
+import World from "../../modules/rpg/worlds/World";
+
+import demoCaveMap from "./data/maps/demoCaveMap.json";
+
+export const main = async ({ settings = {}, start = false } = {}) => {
+	const game = new RPGGame(settings);
+	const world = new World({
+		game,
+		atlas: demoCaveMap,
+		entities: [
+			game.player.entity
+		],
+	});
+
+	console.log(game)
+	console.log(world)
+
+	if(start) {
+		game.start();
+	}
+
+	return {
+		game,
+	};
+};
+
+export default main;
